32f4b54c2dd80c7d7382a0dad8753535943bf1a5
sjc <602205405@qq.com>
2021-03-31 00:37:15 +0800

    this is a vuex test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <h1>{{$store.state.msg}}</h1>
+    <p>{{$store.state.count}}</p> -->
+    <h1>{{msg}}</h1>
+    <h2>{{reverseMsg}}</h2>
+    <p>{{count}}</p>
+    <button type="button" @click="addCount(3)">Mutation</button>
+    <button type="button" @click="increase(5)">Action</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import { mapActions, mapGetters, mapMutations, mapState } from 'vuex' 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // }
+  computed: {
+    // 返回计算属性的方法：count: state => state.count
+    ...mapState(['msg', 'count']),
+    // msg: getters => getters.reverseMsg 
+    ...mapGetters(['reverseMsg']),
+  },
+  methods: {
+    ...mapMutations(['addCount']),
+    ...mapActions(['increase'])
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex from '@/myvuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    msg: 'Hello World',
+    count: 0
+  },
+  getters: {
+    reverseMsg (state) {
+      return state.msg.split('').reverse().join('')
+    }
   },
   mutations: {
+    addCount (state, payload) {
+      state.count += payload
+    }
   },
   actions: {
+    increase (context, payload) {
+      setTimeout(() => {
+        context.commit('addCount', payload)
+      }, 2000)
+    }
   },
   modules: {
   }
